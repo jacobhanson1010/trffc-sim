c1e1dacfa477e2a1f83471aca68ded5907c5a17e
Jacob Hanson <jacob.hanson1010@gmail.com>
2018-12-02 14:01:35 -0600

    iterating with keys and added .2f precision

--- tffc-main.py
+++ tffc-main.py
@@ -1,6 +1,6 @@
 import pygame
 from enum import Enum
-
+import numpy as np
 
 class Status(Enum):
     STOPPED = 'X'
@@ -20,7 +20,7 @@ class Vehicle:
         self.current_speed = current_speed
         self.current_acceleration = acceleration
         self.status = Status.STOPPED
-        if current_speed > 0:
+        if self.current_speed > 0:
             self.status = Status.CRUISING
 
 
@@ -35,66 +35,71 @@ class Lane:
     def process(self):
 
         # brake if car within lookahead distance
-        for current_x in reversed(range(self.length)):
-            if current_x in self.cars:
-                # look ahead within lookahead distance
-                for forward_x in range(self.cars[current_x].lookahead_distance):
-                    lookahead_x = current_x + 1 + forward_x + self.cars[current_x].follow_distance
-                    if lookahead_x in self.cars:
-                        # a car within lookahead+follow distance, set negative acceleration
-                        self.cars[current_x].acceleration = (self.cars[lookahead_x].current_speed - self.cars[current_x].current_speed) / (forward_x + 1)
-                        self.cars[current_x].status = Status.BRAKING
-                        break
+        for current_x in sorted(self.cars.keys(), reverse=True):
+            # look ahead within lookahead distance
+            ahead_x = current_x + .01 \
+                          + self.cars[current_x].lookahead_distance + self.cars[current_x].follow_distance
+            for forward_x in sorted(self.cars.keys(), reverse=True):
+                if current_x < forward_x <= ahead_x:
+                    # a car within lookahead+follow distance, set negative acceleration
+                    self.cars[current_x].status = Status.BRAKING
+                    self.cars[current_x].current_acceleration = (self.cars[ahead_x].current_speed -
+                                                                 self.cars[current_x].current_speed) / forward_x
+                    break
 
         # determine whether cars should speed up
-        for current_x in reversed(range(self.length)):
-            if current_x in self.cars:
-                # look ahead within follow distance
-                should_accelerate = True
-                for forward_x in range(current_x, current_x + self.cars[current_x].follow_distance):
-                    if forward_x + 1 in self.cars:
-                        # a car within follow distance, don't accelerate
-                        self.cars[current_x].current_acceleration = 0
-                        self.cars[current_x].status = Status.CRUISING
-                        should_accelerate = False
-                        break
-                if should_accelerate and self.cars[current_x].status != Status.BRAKING:
-                    # no cars in follow distance, set current acceleration to acceleration attribute
-                    self.cars[current_x].current_acceleration = self.cars[current_x].acceleration
-                    self.cars[current_x].status = Status.ACCELERATING
+        for current_x in sorted(self.cars.keys(), reverse=True):
+            # look ahead within follow distance
+            should_accelerate = True
+            ahead_x = current_x + .01 + self.cars[current_x].follow_distance
+            for forward_x in sorted(self.cars.keys(), reverse=True):
+                if current_x < forward_x <= ahead_x and self.cars[current_x].status == Status.BRAKING:
+                    # a car within follow distance, stop braking
+                    self.cars[current_x].current_acceleration = 0
+                    self.cars[current_x].status = Status.CRUISING
+                    should_accelerate = False
+                    break
+            if should_accelerate and self.cars[current_x].status != Status.BRAKING:
+                # no cars in follow distance, set current acceleration to acceleration attribute
+                self.cars[current_x].current_acceleration = self.cars[current_x].acceleration
+                self.cars[current_x].status = Status.ACCELERATING
 
         # modify speed based on acceleration
-        for current_x in reversed(range(self.length)):
-            if current_x in self.cars:
-                self.cars[current_x].current_speed += self.cars[current_x].current_acceleration
-                if self.cars[current_x].current_speed > self.cars[current_x].top_speed:
-                    self.cars[current_x].current_speed = self.cars[current_x].top_speed
-                    self.cars[current_x].status = Status.CRUISING
+        for current_x in sorted(self.cars.keys(), reverse=True):
+            self.cars[current_x].current_speed += self.cars[current_x].current_acceleration
+
+            if self.cars[current_x].current_speed > self.cars[current_x].top_speed:
+                self.cars[current_x].current_speed = self.cars[current_x].top_speed
+                self.cars[current_x].status = Status.CRUISING
+            if self.cars[current_x].current_speed <= 0:
+                self.cars[current_x].current_speed = 0
+                self.cars[current_x].status = Status.STOPPED
 
         # move cars along
-        for current_x in reversed(range(self.length)):
-            if current_x in self.cars:
-                new_position = current_x + self.cars[current_x].current_speed
-                self.cars[new_position] = self.cars.pop(current_x)
+        for current_x in sorted(self.cars.keys(), reverse=True):
+            new_position = round(current_x + self.cars[current_x].current_speed, 2)
+            self.cars[new_position] = self.cars.pop(current_x)
 
 
 def print_lane(lane_input):
-    for current_x, car in lane_input.cars.items():
-        print(str(car.c_id) + " - x " + str(current_x) + " - speed " + str(car.current_speed))
+    for current_x in sorted(lane_input.cars.keys(), reverse=True):
+        print(str(lane_input.cars[current_x].c_id) + " - x " + str(current_x) +
+              " - speed " + str(lane_input.cars[current_x].current_speed) +
+              " - acceleration " + str(lane_input.cars[current_x].current_acceleration))
 
 
 def print_lane_adv(lane_input):
     top_line = ""
     bottom_line = ""
     cars_line = ""
-    for x in range(lane_input.length):
+    for current_x in range(lane_input.length):
         top_line += "-"
-    for x in range(lane_input.length):
-        if x in lane_input.cars:
-            cars_line += lane_input.cars[x].status.value
+    for current_x in range(lane_input.length):
+        if current_x in lane_input.cars:
+            cars_line += lane_input.cars[current_x].status.value
         else:
             cars_line += " "
-    for x in range(lane_input.length):
+    for current_x in range(lane_input.length):
         bottom_line += "-"
     print(top_line)
     print(cars_line)
@@ -103,19 +108,23 @@ def print_lane_adv(lane_input):
 
 def main():
     print("starting...")
-    lane_1 = Lane(100)
-    for i in range(0, 10, 2):
+    lane_1 = Lane(100.000)
+    #for i in range(0, 10, 2):
         # adda a new vehicle to the lane
-        lane_1.cars[i] = (Vehicle("car" + str(i), 5, 1, 2, 2, 0))
+        #lane_1.cars[i] = Vehicle("car" + str(i/2), 5, 1, 2, 3, 0)
+    lane_1.cars[1] = Vehicle("car" + str(1), 5, 1, 2, 3, 0)
+
+    lane_1.cars[50] = Vehicle("carX", 0, 0, 2, 3, 0)
 
     print("lane_1 at 0")
     print_lane(lane_1)
 
-    for x in range(1, 100):
+    for x in range(1, 15):
         lane_1.process()
         print("lane_1 at " + str(x))
-        print_lane_adv(lane_1)
-        input("Press Enter to continue...")
+        print_lane(lane_1)
+        #print_lane_adv(lane_1)
+        #input("Press Enter to continue...")
 
 
 if __name__ == '__main__':
