222a148be3228f4098e9129cbd9a508e09682824
Jacob Hanson <jacob.hanson1010@gmail.com>
2018-12-15 21:33:25 -0600

    added graphics and refined lane processing

--- tffc-main.py
+++ tffc-main.py
@@ -1,14 +1,15 @@
 import math
-
 import pygame
+import sys
+from pygame.locals import *
 from enum import Enum
-import numpy as np
+
 
 class Status(Enum):
-    STOPPED = 'X'
-    ACCELERATING = 'A'
-    CRUISING = 'C'
-    BRAKING = 'B'
+    STOPPED = (255, 255, 255)
+    ACCELERATING = (0, 255, 0)
+    CRUISING = (255, 255, 0)
+    BRAKING = (255, 0, 0)
 
 
 class Vehicle:
@@ -16,12 +17,12 @@ class Vehicle:
         self.c_id = c_id
         self.top_speed = top_speed
         self.acceleration = acceleration
-        self.follow_distance = follow_distance
+        self.follow_distance = follow_distance + 5
         self.lookahead_distance = lookahead_distance
 
         self.current_speed = current_speed
         self.current_acceleration = acceleration
-        self.status = Status.STOPPED
+        self.status = Status.BRAKING
         if self.current_speed > 0:
             self.status = Status.CRUISING
 
@@ -34,7 +35,27 @@ class Lane:
         # forward length of a lane
         self.length = length
 
+    def get_car_by_id(self, c_id):
+        for x in self.cars.keys():
+            if self.cars[x].c_id == c_id:
+                return self.cars[x]
+
     def process(self):
+        # determine whether cars should speed up
+        for current_x in sorted(self.cars.keys(), reverse=True):
+            # look ahead within follow distance
+            ahead_x = current_x + self.cars[current_x].follow_distance
+            should_accelerate = True
+            for forward_x in sorted(self.cars.keys()):
+                if current_x < forward_x <= ahead_x:
+                    # a car within follow distance, don't accelerate
+                    should_accelerate = False
+                    self.cars[current_x].status = Status.CRUISING
+                    break
+            if should_accelerate:
+                # no cars in follow distance, set current acceleration to acceleration attribute
+                self.cars[current_x].current_acceleration = self.cars[current_x].acceleration
+                self.cars[current_x].status = Status.ACCELERATING
 
         # brake if car within lookahead distance
         for current_x in sorted(self.cars.keys(), reverse=True):
@@ -45,29 +66,15 @@ class Lane:
                     # a car within lookahead+follow distance, set negative acceleration
                     self.cars[current_x].status = Status.BRAKING
                     numerator = (self.cars[forward_x].current_speed - self.cars[current_x].current_speed)
-                    denominator = math.ceil((forward_x - self.cars[current_x].follow_distance - current_x) / self.cars[current_x].current_speed)
+                    denominator = (forward_x - self.cars[current_x].follow_distance - current_x) / self.cars[current_x].current_speed
                     self.cars[current_x].current_acceleration = \
                         numerator / \
                         (denominator, 1)[denominator == 0]
                     break
 
-        # determine whether cars should speed up
-        for current_x in sorted(self.cars.keys(), reverse=True):
-            # look ahead within follow distance
-            ahead_x = current_x + self.cars[current_x].follow_distance
-            should_accelerate = True
-            for forward_x in sorted(self.cars.keys()):
-                if current_x < forward_x <= ahead_x:
-                    # a car within follow distance, don't accelerate
-                    should_accelerate = False
-                    break
-            if should_accelerate and self.cars[current_x].status != Status.BRAKING:
-                # no cars in follow distance, set current acceleration to acceleration attribute
-                self.cars[current_x].current_acceleration = self.cars[current_x].acceleration
-                self.cars[current_x].status = Status.ACCELERATING
-
         # modify speed based on acceleration
         for current_x in sorted(self.cars.keys(), reverse=True):
+
             self.cars[current_x].current_speed += self.cars[current_x].current_acceleration
 
             # set statuses
@@ -85,7 +92,10 @@ class Lane:
         # move cars along
         for current_x in sorted(self.cars.keys(), reverse=True):
             new_position = current_x + self.cars[current_x].current_speed
-            self.cars[new_position] = self.cars.pop(current_x)
+            if new_position >= self.length:
+                self.cars[new_position-self.length] = self.cars.pop(current_x)
+            else:
+                self.cars[new_position] = self.cars.pop(current_x)
 
 
 def print_lane(lane_input):
@@ -115,22 +125,48 @@ def print_lane_adv(lane_input):
 
 def main():
     print("starting...")
-    lane_1 = Lane(100.000)
-    #for i in range(0, 10, 2):
-        # adda a new vehicle to the lane
-        #lane_1.cars[i] = Vehicle("car" + str(i/2), 5, 1, 2, 3, 0)
-    lane_1.cars[1] = Vehicle("car" + str(1), 10, 1, 2, 12, 0)
 
-    lane_1.cars[50] = Vehicle("carX", 2, 1, 2, 3, 0)
+    # set up lanes
+    lanes = [Lane(800)]
 
-    print("lane_1 at 0")
-    print_lane(lane_1)
+    #lanes[0].cars[500] = Vehicle("carX", 0, 2, 5, 12, 0)
 
-    for x in range(1, 150):
-        lane_1.process()
-        print_lane(lane_1)
-        #print_lane_adv(lane_1)
-        #input("Press Enter to continue...")
+    # set up window
+    pygame.init()
+    canvas = pygame.display.set_mode((1000, 500))
+    pygame.display.set_caption("tffc-sim")
+    clock = pygame.time.Clock()
+
+    x = 0
+
+    #for x in range(1, 150):
+    while True:
+        # handle keys
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+            if event.type == KEYDOWN and event.key == K_s:
+                lanes[0].cars[x] = Vehicle("car" + str(x), 10, 1, 10, 50, 0)
+                x += 1
+
+        # wipe display
+        canvas.fill((0, 0, 0))
+
+        # process lanes
+        lanes[0].process()
+        print_lane(lanes[0])
+
+        # draw current lanes
+        for lane in lanes:
+            pygame.draw.rect(canvas, (255, 255, 255), (100, 200, lane.length, 2))
+
+            # draw cars in current lane
+            for current_x in sorted(lane.cars):
+                pygame.draw.rect(canvas, lane.cars[current_x].status.value, (current_x + 100, (200 + 2 / 2) - (5 / 2), 10, 5))
+
+        pygame.display.flip()
+        clock.tick(10)
 
 
 if __name__ == '__main__':
